543d2dcba952fd5560cdc5e236136257aeda41a9
yudai <zeal20410017@fsgcl.jp>
2023-06-28 12:16:52 +0900

    背景画像の追加

--- lib/view/quiz_app.dart
+++ lib/view/quiz_app.dart
@@ -20,41 +20,49 @@ class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          //Columnの中に入れたものは縦に並べられる．Rowだと横に並べられる
-          mainAxisAlignment: MainAxisAlignment.center, //Coloumの中身を真ん中に配置
-          children: <Widget>[
-            const Text(
-              '21年間生き抜いた自分',
-              style: TextStyle(
-                color: Colors.red, //文字の色
-                fontSize: 100, //文字の大きさ
-                fontWeight: FontWeight.bold, //テキストの太さ
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('../assets/image/hikitatenkou.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            //Columnの中に入れたものは縦に並べられる．Rowだと横に並べられる
+            mainAxisAlignment: MainAxisAlignment.center, //Coloumの中身を真ん中に配置
+            children: <Widget>[
+              const Text(
+                '21年間生き抜いた自分',
+                style: TextStyle(
+                  color: Colors.red, //文字の色
+                  fontSize: 100, //文字の大きさ
+                  fontWeight: FontWeight.bold, //テキストの太さ
+                ),
               ),
-            ),
-            Container(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(
-                  onPressed: () {
-                    goToQuizApp(context); //クイズアプリへ遷移するQuizApp関数がよばれる
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white, // 背景色
-                    onPrimary: Colors.red, //ボタンのテキスト文字色
-                    elevation: 10.0,
-                    shadowColor: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0), // 角丸の半径
+              Container(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                    onPressed: () {
+                      goToQuizApp(context); //クイズアプリへ遷移するQuizApp関数がよばれる
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white, // 背景色
+                      onPrimary: Colors.red, //ボタンのテキスト文字色
+                      elevation: 10.0,
+                      shadowColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0), // 角丸の半径
+                      ),
                     ),
-                  ),
-                  child: Text(
-                    'スタート',
-                    style: TextStyle(fontSize: 20),
-                  )),
-            )
-          ],
+                    child: Text(
+                      'スタート',
+                      style: TextStyle(fontSize: 20),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
