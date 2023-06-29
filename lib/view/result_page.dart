f55b4444c0ea4aed729ead77a5b241234c02b5c5
yudai <zeal20410017@fsgcl.jp>
2023-06-29 10:07:00 +0900

    リザルト画面の訂正

--- lib/view/result_page.dart
+++ lib/view/result_page.dart
@@ -34,20 +34,30 @@ class Result extends StatelessWidget {
     }
     print("${result / quizNumber * 100}");
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          //Columnの中に入れたものは縦に並べられる．Rowだと横に並べられる
-          mainAxisAlignment: MainAxisAlignment.center, //Coloumの中身を真ん中に配置
-          children: <Widget>[
-            Text(comment,
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('../assets/image/hikitatenkou2.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent, // Scaffold の背景色を透明に設定
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                comment,
                 style: TextStyle(
-                    fontSize: 100,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red)),
-            Text('正答数$result'),
-            Text('正答率${result / quizNumber * 100}%'),
-            ElevatedButton(
+                  fontSize: 100,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+              Text('正答数$result'),
+              Text('正答率${result / quizNumber * 100}%'),
+              ElevatedButton(
                 onPressed: () async {
                   await goToTop(context);
                 },
@@ -59,8 +69,10 @@ class Result extends StatelessWidget {
                 child: Text(
                   'トップへ戻る',
                   style: TextStyle(color: Colors.red),
-                )),
-          ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
